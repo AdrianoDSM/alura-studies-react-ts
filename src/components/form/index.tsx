5cc49c908ca298179d334fe1283a9f73a9c9813e
Adriano Dias <adriano.dsm1000@gmail.com>
2024-05-23 13:27:43 -0300

    last commit

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,43 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../button";
 import style from './Form.module.scss'
+import { ITarefa } from "../../types/tarefa";
+import {v4 as uuidv4} from 'uuid';
 
-class Form extends React.Component {
-    render() {
-        return (
-            <form className={style.novaTarefa}>
-                <div className={style.inputContainer}>
-                    <label htmlFor="tarefa">
-                        Adicione um novo estudo
-                    </label>
-                    <input 
-                        type='text'
-                        name="tarefa"
-                        id="tarefa"
-                        placeholder="O que você quer estudar"
-                        required
-                    />
-                </div>
-                <div className={style.inputContainer}>
-                    <label>
-                        Tempo
-                    </label>
-                    <input
-                        type="time"
-                        step='1'
-                        name="tempo"
-                        id="tempo"
-                        min='00:00:00'
-                        max='01:30:00'
-                        required
-                    />
-                </div>
-                <Button>
-                    Adicionar
-                </Button>
-            </form>
-        )
+interface Props {
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
+
+function Form ({setTarefas}: Props) {
+    const[tarefa, setTarefa]=useState('')
+    const[tempo, setTempo]=useState('00:00')
+    function adicionarTarefa(e: React.FormEvent){
+        e.preventDefault();
+        setTarefas(tarefasAntigas => [
+            ...tarefasAntigas, 
+            {
+                tarefa, 
+                tempo, 
+                selecionado: false, 
+                completado: false, 
+                id: uuidv4()
+            }
+        ])
+        setTarefa('')
+        setTempo('00:00')
     }
+    return (
+        <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+            <div className={style.inputContainer}>
+                <label htmlFor="tarefa">
+                    Adicione um novo estudo
+                </label>
+                <input 
+                    type='text'
+                    name="tarefa"
+                    id="tarefa"
+                    value={tarefa}
+                    onChange={evento => setTarefa(evento.target.value)}
+                    placeholder="O que você quer estudar"
+                    required
+                />
+            </div>
+            <div className={style.inputContainer}>
+                <label>
+                    Tempo
+                </label>
+                <input
+                    type="time"
+                    step='1'
+                    name="tempo"
+                    value={tempo}
+                    onChange={evento => setTempo(evento.target.value)}
+                    id="tempo"
+                    min='00:00:00'
+                    max='01:30:00'
+                    required
+                />
+            </div>
+            <Button type='submit'>Adicionar</Button>
+        </form>
+    )
 }
 
 export default Form;
--- src/components/stopwatch/index.tsx
+++ src/components/stopwatch/index.tsx
@@ -1,15 +1,40 @@
+import { useEffect, useState } from "react";
+import { tempoParaSegundo } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 import Button from "../button";
 import Clock from "./clock";
 import styles from './Stopwatch.module.scss'
 
-export default function Stopwatch(){
+interface Props {
+    selecionado: ITarefa | undefined,
+    finalizarTarefa: () => void
+}
+
+export default function Stopwatch({ selecionado, finalizarTarefa }: Props) {
+    const [tempo, setTempo] = useState<number>()
+
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundo(selecionado?.tempo))
+        }
+    }, [selecionado])
+
+    function regressiva(contador: number = 0) {
+        setTimeout(()=>{
+            if(contador>0){
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+            finalizarTarefa();
+        },1000)
+    }
     return (
         <div className={styles.cronometro}>
             <p className={styles.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={styles.relogioWrapper}>
-                <Clock/>
+                <Clock tempo={tempo}/>
             </div>
-            <Button>Começar!</Button>
+            <Button onClick={()=> regressiva(tempo)}>Começar!</Button>
         </div>
     )
 }
